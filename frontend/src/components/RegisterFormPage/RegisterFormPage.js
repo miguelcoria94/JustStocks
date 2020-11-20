0352a244fdfa1a8243f5c0b860a22f853ca21a32
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-20 17:19:41 -0600

    fixed regisre

--- frontend/src/components/RegisterFormPage/RegisterFormPage.js
+++ frontend/src/components/RegisterFormPage/RegisterFormPage.js
@@ -32,52 +32,56 @@ function RegisterFormPage() {
 
     return (
       <div className="registerform_container">
-        <form onSubmit={handleSubmit}>
-        <ul>
-            {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-            ))}
-        </ul>
-        <label>
-            Email
-            <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            />
-        </label>
-        <label>
-            Username
-            <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            />
-        </label>
-        <label>
-            Password
-            <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            />
-        </label>
-        <label>
-            Confirm Password
-            <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-            />
-        </label>
-        <button type="submit">Sign Up</button>
+        <form onSubmit={handleSubmit} className="registerhome_content-form">
+          <div className="registerform">
+            <ul>
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+            <h1 className="registerform_title">JustStocks</h1>
+            <h2 className="registerform_subheading">Signin</h2>
+            <label>
+              Email
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Username
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Password
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Confirm Password
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </label>
+            <button type="submit">Sign Up</button>
+          </div>
         </form>
-    </div>
-  );
+      </div>
+    );
 }
 
 export default RegisterFormPage;
