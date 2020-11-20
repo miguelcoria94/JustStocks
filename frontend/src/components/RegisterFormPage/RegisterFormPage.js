af9f4145454aa7e7d37d6cc4497d6f08d4c1a1b9
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-20 17:13:45 -0600

    form is inside the div

--- frontend/src/components/RegisterFormPage/RegisterFormPage.js
+++ frontend/src/components/RegisterFormPage/RegisterFormPage.js
@@ -30,51 +30,53 @@ function RegisterFormPage() {
     ]);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    return (
+      <div className="registerform_container">
+        <form onSubmit={handleSubmit}>
+        <ul>
+            {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+            ))}
+        </ul>
+        <label>
+            Email
+            <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            />
+        </label>
+        <label>
+            Username
+            <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            />
+        </label>
+        <label>
+            Password
+            <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+        </label>
+        <label>
+            Confirm Password
+            <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            />
+        </label>
+        <button type="submit">Sign Up</button>
+        </form>
+    </div>
   );
 }
 
