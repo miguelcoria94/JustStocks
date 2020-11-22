f9119602c598ed9078dcd216a7500e78cfc9011d
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-21 23:15:24 -0600

    create profile page route and folder

---
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, Link } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import "./ProfilePage.style.css";
+
+function ProfilePage() {
+
+}
+
+export default ProfilePage;
+
