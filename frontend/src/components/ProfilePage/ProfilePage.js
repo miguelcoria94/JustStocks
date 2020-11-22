c4064bddef202c57906e3959725cf03415155bea
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-21 23:57:14 -0600

    prevent logout user from seeing profile

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LogoutButton from "./LogoutButton";
 import "./ProfilePage.style.css";
 
 function ProfilePage({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
+
+    if (!sessionUser) return <Redirect to="/" />;
 
     return (
       <div>
