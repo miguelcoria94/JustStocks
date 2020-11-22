acafb328ce8aa22d215dfaf74a2d7939dea96f16
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-22 00:38:10 -0600

    finish readme

--- frontend/src/components/ProfilePage/LogoutButton.js
+++ frontend/src/components/ProfilePage/LogoutButton.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 
 function LogoutButton({ user }) {
   const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
 
   const logout = (e) => {
     e.preventDefault();
