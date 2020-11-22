643c259dcb8018ff2cb01d7c336e5fa52a44c93d
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-21 20:57:47 -0600

    fix title width

--- frontend/src/components/DefaultHomePage/DefaultHomePage.js
+++ frontend/src/components/DefaultHomePage/DefaultHomePage.js
@@ -32,75 +32,75 @@ function DefaultHomePage() {
     };
 
     return (
-        <div className="HomeDefault_container">
-            <div className="DefaultHome_title">
-                <h1>TestLOGO</h1>
-            </div>
-            <div className="registerform_container">
-                <form onSubmit={handleSubmit} className="registerhome_content-form">
-                    <div className="registerform">
-                    <div>
-                        <div className="register-login-error-box">
-                        {errors.map((error, idx) => (
-                            <li key={idx}>{error}</li>
-                        ))}
-                        </div>
-                    </div>
-                    <h1>
-                        <Link className="registerform_title" to="/">
-                        JustStocks
-                        </Link>
-                    </h1>
-                    <h2 className="registerform_subheading">Register</h2>
-                    <div className="form_input-container">
-                        <label>Email</label>
-                        <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="form_input-container">
-                        <label>Username</label>
-                        <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className="form_input-container">
-                        <label>Password</label>
-                        <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="form_input-container">
-                        <label>Confirm Password</label>
-                        <input
-                        type="password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className="form_input-container">
-                        <button className="registerbtn" type="submit">
-                        Register
-                        </button>
-                    </div>
-                    </div>
-                </form>
-                <span>
-                    Already a member?{" "}
-                    <Link to="/login" className="register_link">
-                    Login
-                    </Link>
-                </span>
-            </div>
+      <div className="HomeDefault_container">
+        <div className="DefaultHome_container">
+          <h1 className="DefaultHome_title">What we offer</h1>
         </div>
-    )
+        <div className="registerform_container">
+          <form onSubmit={handleSubmit} className="registerhome_content-form">
+            <div className="registerform">
+              <div>
+                <div className="register-login-error-box">
+                  {errors.map((error, idx) => (
+                    <li key={idx}>{error}</li>
+                  ))}
+                </div>
+              </div>
+              <h1>
+                <Link className="registerform_title" to="/">
+                  JustStocks
+                </Link>
+              </h1>
+              <h2 className="registerform_subheading">Register</h2>
+              <div className="form_input-container">
+                <label>Email</label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form_input-container">
+                <label>Username</label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="form_input-container">
+                <label>Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="form_input-container">
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <div className="form_input-container">
+                <button className="registerbtn" type="submit">
+                  Register
+                </button>
+              </div>
+            </div>
+          </form>
+          <span>
+            Already a member?{" "}
+            <Link to="/login" className="register_link">
+              Login
+            </Link>
+          </span>
+        </div>
+      </div>
+    );
 }
 
 export default DefaultHomePage
