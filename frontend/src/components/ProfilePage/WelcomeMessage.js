913c3b20ca18909d7a77fdfbfeea8cbf6e58e9e8
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-22 00:43:18 -0600

    create file for message

---
+++ frontend/src/components/ProfilePage/WelcomeMessage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as sessionActions from "../../store/session";
+
+function WelcomeMessage({ user }) {
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default WelcomeMessage;
