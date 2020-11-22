534a2ad00413c9a03679be5d6e2db487597862b7
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-22 08:06:23 -0600

    create a searchbar file

---
+++ frontend/src/components/ProfilePage/SearchBar.js
@@ -0,0 +1,37 @@
+import React { useState }from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/profile";
+
+function SearchBar() {
+    const dispatch = useDispatch();
+    const [symbol, setSymbol] = useState("");
+    
+  const handleSearch = (e) => {
+      e.preventDefault();
+      return dispatch(
+          sessionActions.lookUp({ symbol })
+      )
+  };
+
+  return (
+    <>
+      <form onClick={handleSearch} to="/profile" className="profile-logout-button">
+        <div className="form_input-container">
+                  <input
+                      placeholder="Search for..."
+            type="text"
+            value={symbol}
+            onChange={(e) => setSymbol(e.target.value)}
+          />
+        </div>
+        <div className="form_input-container">
+          <button className="registerbtn" type="submit">
+            Register
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default SearchBar;
