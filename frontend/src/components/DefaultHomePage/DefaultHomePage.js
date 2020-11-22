6c59ccfd196432617d18261776c2c1bb31597787
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-21 20:39:45 -0600

    create homefault page

---
+++ frontend/src/components/DefaultHomePage/DefaultHomePage.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, Link } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import "./DefaultHomePage.style.css";
+
+
+function DefaultHomePage() {
+    
+}
