be1ba1e03ae70fe51eff713ab1cf3a0f003ec7e5
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-21 23:55:50 -0600

    logout button is working

--- frontend/src/components/ProfilePage/LogoutButton.js
+++ frontend/src/components/ProfilePage/LogoutButton.js
@@ -6,23 +6,6 @@ function LogoutButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = () => {
-      setShowMenu(false);
-    };
-
-    document.addEventListener("click", closeMenu);
-
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
-
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
@@ -30,18 +13,9 @@ function LogoutButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
+      <button onClick={logout} to="/">
+        <i className="fas fa-user-circle" /> Logout
       </button>
-      {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      )}
     </>
   );
 }
