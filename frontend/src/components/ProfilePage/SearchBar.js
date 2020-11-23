3b5263a22d8bed897be6d39a3a7c2d7c75469660
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-23 02:59:33 -0600

    add search result div

--- frontend/src/components/ProfilePage/SearchBar.js
+++ frontend/src/components/ProfilePage/SearchBar.js
@@ -39,12 +39,12 @@ function SearchBar() {
               Search
             </button>
             {searchResult ? (
-              <div>
-                <li>{`${searchResult[0]["1. symbol"]} ${searchResult[0]["2. name"]}`}</li>
-                <li>{`${searchResult[1]["1. symbol"]} ${searchResult[1]["2. name"]}`}</li>
-                <li>{`${searchResult[2]["1. symbol"]} ${searchResult[2]["2. name"]}`}</li>
-                <li>{`${searchResult[4]["1. symbol"]} ${searchResult[3]["2. name"]}`}</li>
-                <li>{`${searchResult[5]["1. symbol"]} ${searchResult[4]["2. name"]}`}</li>
+              <div className="search-result-div">
+                <li>{`$${searchResult[0]["1. symbol"]} - ${searchResult[0]["2. name"]}`}</li>
+                <li>{`$${searchResult[1]["1. symbol"]} - ${searchResult[1]["2. name"]}`}</li>
+                <li>{`$${searchResult[2]["1. symbol"]} - ${searchResult[2]["2. name"]}`}</li>
+                <li>{`$${searchResult[4]["1. symbol"]} - ${searchResult[3]["2. name"]}`}</li>
+                <li>{`$${searchResult[5]["1. symbol"]} - ${searchResult[4]["2. name"]}`}</li>
               </div>
             ) : (
               <li></li>
