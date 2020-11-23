e874c95aae22fc78a707c258aeeb35a485ca3353
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-23 06:32:08 -0600

    remove un-used code from searchbar

--- frontend/src/components/ProfilePage/SearchBar.js
+++ frontend/src/components/ProfilePage/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { useState }from "react";
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch } from "react-redux";
 import * as profileActions from "../../store/profile";
 import './ProfilePage.style.css'
 import SearchItem from './SearchItem'
