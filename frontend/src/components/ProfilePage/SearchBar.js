8c54f6efd0c39d534ccdb3816c1aa5123f8b1586
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-23 03:15:42 -0600

    set up li for search

--- frontend/src/components/ProfilePage/SearchBar.js
+++ frontend/src/components/ProfilePage/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState }from "react";
 import { useDispatch, useSelector} from "react-redux";
 import * as profileActions from "../../store/profile";
+import './ProfilePage.style.css'
 
 
 function SearchBar() {
