c8cd241461171625b59adace2059186f767e3fe9
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-23 07:59:39 -0600

    styling data div

--- frontend/src/components/ProfilePage/StockData.js
+++ frontend/src/components/ProfilePage/StockData.js
@@ -11,10 +11,18 @@ function StockData({ stock }) {
     console.log(stockDetails)
 
     return (
-        <div>
-            { (stockDetails.length > 0) ? <h3>{stockDetails[0][0]}</h3> : <h1>Loading...</h1>}
-        </div>
-    )
+      <div>
+        {stockDetails.length > 0 ? (
+          <div className="stock-data-div">
+            <h3>{`Symbol - ${stockDetails[0][0].slice(1)}`}</h3>
+            <p>{`Name - ${stockDetails[0][2].slice(1)}`}</p>
+            <p>{`Description - ${stockDetails[0][3].slice(1)}`}</p>
+          </div>
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </div>
+    );
 }
 
 export default StockData;
