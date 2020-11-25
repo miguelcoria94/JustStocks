078ad1e791020dda4f52e5958ecb2549d79df617
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-25 14:47:09 -0600

    create stock table

---
+++ backend/db/migrations/20201125204254-create-stock.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Stocks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      symbol: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Stocks');
+  }
+};
