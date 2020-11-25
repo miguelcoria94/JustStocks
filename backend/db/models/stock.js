fcb058925af459c845463bddf0d9c2f540f5eb23
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-25 15:12:50 -0600

    create associations

--- backend/db/models/stock.js
+++ backend/db/models/stock.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      const cloumnMapping = {
+        through: "WatchList",
+        otherKey: "userId",
+        foreignKey: "stockId",
+      };
+      this.belongsToMany(models.User, cloumnMapping);
     }
   };
   Stock.init({
