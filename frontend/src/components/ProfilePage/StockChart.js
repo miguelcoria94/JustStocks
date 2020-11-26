094fadc12d1188a3f6bbc175aac7721d87dc2136
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-26 01:35:19 -0600

    add open price

--- frontend/src/components/ProfilePage/StockChart.js
+++ frontend/src/components/ProfilePage/StockChart.js
@@ -14,24 +14,17 @@ function StockChart({ graphData }) {
   console.log("ss",chartData)
 
   return (
-    <div style={{ width: "100%", height: 300 }}>
+    <div className="graph-container" style={{ width: "100%", height: 300 }}>
       <ResponsiveContainer>
         <LineChart
-          width={500}
           height={100}
           data={chartData}
-          margin={{ top: 20, right: 0, left: 0, bottom: 0 }}
+          margin={{ top: 0, right: 0, left: 0, bottom: 10 }}
         >
           <Tooltip />
-          <Legend />
-          <XAxis dataKey="2. high" />
-          <YAxis type="number" domain={"auto"} />
-          <Line
-            dataKey="2. high"
-            stroke="#169E5A"
-            strokeWidth={5}
-            fill="#8884d8"
-          />
+          <YAxis domain={"auto"} hide="true" />
+          <Line dataKey="1. open" stroke="#169E5A" strokeWidth={5} />
+          <Line dataKey="2. high" stroke="#169E5A" strokeWidth={5} />
           <Line dataKey="3. low" stroke="#227956" strokeWidth={5} />
         </LineChart>
       </ResponsiveContainer>
