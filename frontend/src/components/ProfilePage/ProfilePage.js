72476f87db31418bb3ba1199805b43115f9ea307
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-27 10:19:40 -0600

    got watchlist to load on login

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -7,13 +7,20 @@ import SearchBar from "./SearchBar"
 import StockData from "./StockData"
 import StockChart from "./StockChart"
 import "./ProfilePage.style.css";
+import { useDispatch } from "react-redux";
+import * as profileActions from "../../store/profile";
 
 function ProfilePage() {
-
+  
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
   const searchBar = useSelector((state) => state.profile.symbol);
   const stockData = useSelector((state) => state.profile.stock);
   const graphData = useSelector((state) => state.profile.graph);
+
+  if (sessionUser) {
+    dispatch(profileActions.getWatchlist({sessionUser}))
+  }
 
   if (sessionUser) {
     document.title = `JustStocks - ${sessionUser.username}`
