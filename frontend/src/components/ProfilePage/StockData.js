de073377e58f13ba6b5dd755c244eec33bd3f1d8
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-29 15:56:25 -0600

    fix buttons made them forms to submit

--- frontend/src/components/ProfilePage/StockData.js
+++ frontend/src/components/ProfilePage/StockData.js
@@ -21,12 +21,12 @@ function StockData({ stock, search, user }) {
         stockDetails.push(stockData)
     }
   
-  const addStockToWatchlist = () => {
-    console.log("add", stock.Symbol, user.id)
+  const addStockToWatchlist = (symbol) => {
+    console.log("add", symbol, user.id)
   }
 
-  const removeStockToWatchlist = () => {
-    console.log("remove")
+  const removeStockToWatchlist = (symbol) => {
+    console.log("remove", symbol, user.id)
   };
 
     return (
@@ -34,15 +34,26 @@ function StockData({ stock, search, user }) {
         {stockDetails.length > 0 ? (
           <div className="stock-data-div">
             <div className="featured-data">
-              <button className="add-button" onClick={addStockToWatchlist}>
-                Add to watchlist
-              </button>
-              <button
-                className="remove-button"
-                onClick={removeStockToWatchlist}
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  addStockToWatchlist(stockDetails[0][0].slice(1)[0]);
+                }}
               >
-                Remove from watchlist
-              </button>
+                <button type="submit" className="add-button">
+                  Add to watchlist
+                </button>
+              </form>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  removeStockToWatchlist(stockDetails[0][0].slice(1)[0]);
+                }}
+              >
+                <button type="submit" className="remove-button">
+                  Remove from watchlist
+                </button>
+              </form>
             </div>
             <h1>{`$${stockDetails[0][0].slice(1)} Stock Overview`}</h1>
             <div className="featured-data">
@@ -86,62 +97,73 @@ function StockData({ stock, search, user }) {
             </div>
             <p>{`${stockDetails[0][3].slice(1)}`}</p>
           </div>
-        ) : (firstStockDetails.length > 0) ? (
-            <div className="stock-data-div">
-              <div className="featured-data">
-                <button className="add-button" onClick={addStockToWatchlist}>
+        ) : firstStockDetails.length > 0 ? (
+          <div className="stock-data-div">
+            <div className="featured-data">
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  addStockToWatchlist(firstStockDetails[0][0].slice(1)[0]);
+                }}
+              >
+                <button type="submit" className="add-button">
                   Add to watchlist
                 </button>
-                <button
-                  className="remove-button"
-                  onClick={removeStockToWatchlist}
-                >
+              </form>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  removeStockToWatchlist(firstStockDetails[0][0].slice(1)[0]);
+                }}
+              >
+                <button type="submit" className="remove-button">
                   Remove from watchlist
                 </button>
-              </div>
-              <h1>{`$${firstStockDetails[0][0].slice(1)} Stock Overview`}</h1>
-              <div className="featured-data">
-                <h3 className="market-cap">{`Market Cap - ${numeral(
-                  Number(firstStockDetails[0][13].slice(1))
-                )
-                  .format("($ 0.00 a)")
-                  .toUpperCase()}`}</h3>
-              </div>
-              <div className="featured-data">
-                <p className="featured-data-text">
-                  <span className="featured-data-title">Name</span>
-                  {` - ${firstStockDetails[0][2].slice(1)}`}
-                </p>
-                <p className="featured-data-text">
-                  <span className="featured-data-title">Exchange</span>
-                  {` - ${firstStockDetails[0][4].slice(1)}`}
-                </p>
-                <p className="featured-data-text">
-                  <span className="featured-data-title">Sector</span>
-                  {` - ${firstStockDetails[0][8].slice(1)}`}
-                </p>
-              </div>
-              <div className="featured-data">
-                <p className="featured-data-text">
-                  <span className="featured-data-title">EPS</span>
-                  {` - ${firstStockDetails[0][20].slice(1)}`}
-                </p>
-                <p className="featured-data-text">
-                  <span className="featured-data-title">52WeekHigh</span>
-                  {` - ${numeral(Number(firstStockDetails[0][39].slice(1)))
-                    .format("($ 0.00 a)")
-                    .toUpperCase()}`}
-                </p>
-                <p className="featured-data-text">
-                  <span className="featured-data-title">52WeekLow</span>
-                  {` - ${numeral(Number(firstStockDetails[0][40].slice(1)))
-                    .format("($ 0.00 a)")
-                    .toUpperCase()}`}
-                </p>
-              </div>
-              <p>{`${firstStockDetails[0][3].slice(1)}`}</p>
+              </form>
             </div>
-          ) : (
+            <h1>{`$${firstStockDetails[0][0].slice(1)} Stock Overview`}</h1>
+            <div className="featured-data">
+              <h3 className="market-cap">{`Market Cap - ${numeral(
+                Number(firstStockDetails[0][13].slice(1))
+              )
+                .format("($ 0.00 a)")
+                .toUpperCase()}`}</h3>
+            </div>
+            <div className="featured-data">
+              <p className="featured-data-text">
+                <span className="featured-data-title">Name</span>
+                {` - ${firstStockDetails[0][2].slice(1)}`}
+              </p>
+              <p className="featured-data-text">
+                <span className="featured-data-title">Exchange</span>
+                {` - ${firstStockDetails[0][4].slice(1)}`}
+              </p>
+              <p className="featured-data-text">
+                <span className="featured-data-title">Sector</span>
+                {` - ${firstStockDetails[0][8].slice(1)}`}
+              </p>
+            </div>
+            <div className="featured-data">
+              <p className="featured-data-text">
+                <span className="featured-data-title">EPS</span>
+                {` - ${firstStockDetails[0][20].slice(1)}`}
+              </p>
+              <p className="featured-data-text">
+                <span className="featured-data-title">52WeekHigh</span>
+                {` - ${numeral(Number(firstStockDetails[0][39].slice(1)))
+                  .format("($ 0.00 a)")
+                  .toUpperCase()}`}
+              </p>
+              <p className="featured-data-text">
+                <span className="featured-data-title">52WeekLow</span>
+                {` - ${numeral(Number(firstStockDetails[0][40].slice(1)))
+                  .format("($ 0.00 a)")
+                  .toUpperCase()}`}
+              </p>
+            </div>
+            <p>{`${firstStockDetails[0][3].slice(1)}`}</p>
+          </div>
+        ) : (
           <h1>Loading...</h1>
         )}
       </div>
