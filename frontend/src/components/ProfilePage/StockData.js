60cf34e5ef5075dabf41a77c7c962f92011f7ac4
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-29 18:38:55 -0600

    fix loading bug

--- frontend/src/components/ProfilePage/StockData.js
+++ frontend/src/components/ProfilePage/StockData.js
@@ -131,50 +131,100 @@ function StockData({ stock, search, user }) {
                 </button>
               </form>
             </div>
-            <h1>{`$${firstStockDetails[0][0].slice(1)} Stock Overview`}</h1>
+            <h1>
+              {firstStockDetails.length < 0
+                ? `$${firstStockDetails[0][0].slice(1)} Stock Overview`
+                : "Waiting for user..."
+                ? `$${firstStockDetails[0][0].slice(1)} Stock Overview`
+                : "Waiting for user..."}
+            </h1>
             <div className="featured-data">
-              <h3 className="market-cap">{`Market Cap - ${numeral(
-                Number(firstStockDetails[0][13].slice(1))
-              )
-                .format("($ 0.00 a)")
-                .toUpperCase()}`}</h3>
+              <h3 className="market-cap">
+                {firstStockDetails.length < 0
+                  ? `Market Cap - ${numeral(
+                      Number(firstStockDetails[0][13].slice(1))
+                    )
+                      .format("($ 0.00 a)")
+                      .toUpperCase()}`
+                  : "Waiting for user..."
+                  ? `Market Cap - ${numeral(
+                      Number(firstStockDetails[0][13].slice(1))
+                    )
+                      .format("($ 0.00 a)")
+                      .toUpperCase()}`
+                  : "Waiting for user..."}
+              </h3>
             </div>
             <div className="featured-data">
               <p className="featured-data-text">
                 <span className="featured-data-title">Name</span>
-                {` - ${firstStockDetails[0][2].slice(1)}`}
+                {firstStockDetails.length < 0
+                  ? ` - ${firstStockDetails[0][2].slice(1)}`
+                  : "Waiting for user..."
+                  ? ` - ${firstStockDetails[0][2].slice(1)}`
+                  : "Waiting for user..."}
               </p>
               <p className="featured-data-text">
                 <span className="featured-data-title">Exchange</span>
-                {` - ${firstStockDetails[0][4].slice(1)}`}
+                {firstStockDetails.length < 0
+                  ? ` - ${firstStockDetails[0][4].slice(1)}`
+                  : "Waiting for user..."
+                  ? ` - ${firstStockDetails[0][4].slice(1)}`
+                  : "Waiting for user..."}
               </p>
               <p className="featured-data-text">
                 <span className="featured-data-title">Sector</span>
-                {` - ${firstStockDetails[0][8].slice(1)}`}
+                {firstStockDetails.length < 0
+                  ? ` - ${firstStockDetails[0][8].slice(1)}`
+                  : "Waiting for user..."
+                  ? ` - ${firstStockDetails[0][8].slice(1)}`
+                  : "Waiting for user..."}
               </p>
             </div>
             <div className="featured-data">
               <p className="featured-data-text">
                 <span className="featured-data-title">EPS</span>
-                {` - ${firstStockDetails[0][20].slice(1)}`}
+                {firstStockDetails.length < 0
+                  ? ` - ${firstStockDetails[0][20].slice(1)}`
+                  : "Waiting for user..."
+                  ? ` - ${firstStockDetails[0][20].slice(1)}`
+                  : "Waiting for user..."}
               </p>
               <p className="featured-data-text">
                 <span className="featured-data-title">52WeekHigh</span>
-                {` - ${numeral(Number(firstStockDetails[0][39].slice(1)))
-                  .format("($ 0.00 a)")
-                  .toUpperCase()}`}
+                {firstStockDetails.length < 0
+                  ? ` - ${numeral(Number(firstStockDetails[0][39].slice(1)))
+                      .format("($ 0.00 a)")
+                      .toUpperCase()}`
+                  : "Waiting for user..."
+                  ? ` - ${numeral(Number(firstStockDetails[0][39].slice(1)))
+                      .format("($ 0.00 a)")
+                      .toUpperCase()}`
+                  : "Waiting for user..."}
               </p>
               <p className="featured-data-text">
                 <span className="featured-data-title">52WeekLow</span>
-                {` - ${numeral(Number(firstStockDetails[0][40].slice(1)))
-                  .format("($ 0.00 a)")
-                  .toUpperCase()}`}
+                {firstStockDetails.length < 0
+                  ? ` - ${numeral(Number(firstStockDetails[0][40].slice(1)))
+                      .format("($ 0.00 a)")
+                      .toUpperCase()}`
+                  : "Waiting for user..."
+                  ? ` - ${numeral(Number(firstStockDetails[0][40].slice(1)))
+                      .format("($ 0.00 a)")
+                      .toUpperCase()}`
+                  : "Waiting for user..."}
               </p>
             </div>
-            <p>{`${firstStockDetails[0][3].slice(1)}`}</p>
+            <p>
+              {firstStockDetails.length < 0
+                ? `${firstStockDetails[0][3].slice(1)}`
+                : "Waiting for user..."
+                ? `${firstStockDetails[0][3].slice(1)}`
+                : "Waiting for user..."}
+            </p>
           </div>
         ) : (
-          <h1>Loading...</h1>
+          <h1>Waiting for user...</h1>
         )}
       </div>
     );
