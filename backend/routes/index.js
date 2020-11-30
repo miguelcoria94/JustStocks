340f671786022c697c5ad3e5b3431a33367af496
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-29 20:17:52 -0600

    set up heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,18 +16,18 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/build")));
+  router.use(express.static("../frontend/build"));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    return sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
